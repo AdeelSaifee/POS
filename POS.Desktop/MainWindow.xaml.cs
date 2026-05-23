6431cb9a9ce2351d236a901663da099ebc734c8f
Adeel <adeel.saifee822@gmail.com>
2026-05-23 10:48:38 +0500

    feat(ui): integrate WebViewHost into MainWindow and update manager details in UI prototype

--- POS.Desktop/MainWindow.xaml.cs
+++ POS.Desktop/MainWindow.xaml.cs
@@ -1,13 +1,5 @@
-﻿using System.Text;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
+using POS.Desktop.Shell;
 
 namespace POS.Desktop
 {
@@ -16,9 +8,12 @@ namespace POS.Desktop
     /// </summary>
     public partial class MainWindow : Window
     {
+        private readonly WebViewHost _webViewHost;
+
         public MainWindow()
         {
             InitializeComponent();
+            _webViewHost = new WebViewHost(MainWebView);
         }
     }
 }
